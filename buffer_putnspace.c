f9cab2edf61fc832b90055e343192c5c59837ff2
Roman Senn <roman.l.senn@gmail.com>
2013-07-31 08:49:15 +0200

    ...

--- buffer_putnspace.c
+++ buffer_putnspace.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 //#ifdef HAVE_ALLOCA_H
-#include <alloca.h>
+//#include <alloca.h>
 //#endif /* HAVE_ALLOCA_H */
 #include "str.h"
 #include "byte.h"
@@ -8,10 +8,11 @@
 
 int buffer_putnspace(buffer* b, int n) 
 {
-  char *space;
   if(n <= 0)
     return 0;
-  space = alloca(n);
+	{ 
+					char space[n];
   byte_fill(space, n, ' ');
   return buffer_put(b,space,n);
+	}
 }
