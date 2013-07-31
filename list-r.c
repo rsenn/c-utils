daaa939ee12b32beae2431e02a2082e359260b46
Roman Senn <roman.l.senn@gmail.com>
2013-07-31 03:44:16 +0200

    ...

--- list-r.c
+++ list-r.c
@@ -6,35 +6,66 @@
 #include "stralloc.h"
 static char buffer_1_out[BUFFER_OUTSIZE];
 static buffer buffer_1 = BUFFER_INIT(write, 1, buffer_1_out, BUFFER_OUTSIZE);
+int list_dir_internal(stralloc *dir,  char type);
 
 int list_dir(stralloc *dir)
 {
-				DIR *d;
+        list_dir_internal(dir, DT_DIR);
+        list_dir_internal(dir, -DT_DIR);
+}
+
+int list_dir_internal(stralloc *dir,  char type)
+{
+        DIR *d;
         struct dirent *de;
-				char *idx;
+        char *idx;
+        unsigned long l;
 
-				stralloc_zero(dir);
-			  d	= opendir(dir->s);
-				idx = dir->s + dir->len;
-				stralloc_cats(dir, "/");
+        stralloc_nul(dir);
+        d  = opendir(dir->s);
+        stralloc_cats(dir, "/");
+        l = dir->len;
+        while((de = readdir(d)))
+        {
+          dir->len = l;
 
-				while((de = readdir(d)))
-				{
-							idx[1] = '\0';
+          if(strcmp(de->d_name, ".") == 0) continue;
+          if(strcmp(de->d_name, "..") == 0) continue;
 
-							stralloc_cats(dir, de->d_name);
+          if(type > 0 && !(de->d_type & type)) continue;
+          if(type < 0 && (de->d_type & (-type))) continue;
 
-								buffer_put(&buffer_1, dir->s,dir->len);
-								buffer_put(&buffer_1, "\n", 1);
-				buffer_flush(&buffer_1);
-				}
-				closedir(d);
+//        idx = dir->s + l;  *idx = '\0';
+
+         stralloc_readyplus(dir, strlen(de->d_name)+1);
+         strcpy(dir->s + dir->len, de->d_name);
+         dir->len+=strlen(de->d_name);
+        //      stralloc_cats(dir, de->d_name);
+        //
+
+        if(de->d_type & DT_DIR)
+        {
+                stralloc_cats(dir, "/");
+        }
+     
+
+                buffer_put(&buffer_1, dir->s,dir->len);
+                buffer_put(&buffer_1, "\n", 1);
+             buffer_flush(&buffer_1);
+      
+          if(de->d_type & DT_DIR)
+          {
+                dir->len--;
+                  list_dir_internal(dir,0);
+          }
+        }
+        closedir(d);
 }
 
 int main(int argc, char *argv[])
 {
-				stralloc dir={0,0,0};
-				stralloc_copys(&dir,argv[1]);
-				list_dir(&dir);
-				return 0;
+        stralloc dir={0,0,0};
+        stralloc_copys(&dir,argv[1]);
+        list_dir_internal(&dir,0);
+        return 0;
 }
