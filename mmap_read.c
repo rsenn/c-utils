1c5139d343dfaa8c75a0bc707b74751078806dde
Roman Senn <roman.l.senn@gmail.com>
2013-11-29 14:52:01 +0100

    ...

--- mmap_read.c
+++ mmap_read.c
@@ -1,6 +1,6 @@
 #include <sys/types.h>
 #include <unistd.h>
-#if defined(__MINGW32__) || defined(__MSYS__)
+#ifdef __MINGW32__
 #include <windows.h>
 #else
 #include <sys/mman.h>
@@ -10,7 +10,7 @@
 
 extern char* mmap_read(const char* filename,unsigned long* filesize) {
   char *map;
-#if defined(__MINGW32__) || defined(__MSYS__)
+#ifdef __MINGW32__
 #define close CloseHandle
   HANDLE fd;
   fd=CreateFile(filename,GENERIC_READ,FILE_SHARE_READ,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0);
@@ -21,7 +21,7 @@ extern char* mmap_read(const char* filename,unsigned long* filesize) {
   if(fd == -1)
     return 0;
 #endif
-  map = mmap_read_fd((int)fd, filesize);
+  map = mmap_read_fd(fd, filesize);
   close(fd);
   return map;
 }
