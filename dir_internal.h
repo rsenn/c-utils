61eabad84d0b4f79c5ae2917497938fb8cd0e27a
Roman Senn <roman.l.senn@gmail.com>
2014-06-22 07:49:02 +0000

    ...

--- dir_internal.h
+++ dir_internal.h
@@ -42,5 +42,12 @@ struct dir_internal_s {
 #endif
 
 
+#if defined(__MINGW32__) || defined(__MSYS__)
+#define dir_ISLINK(d) (!!(dir_ATTRS(d)&0x08))
+#else
+#define dir_ISLINK(d) S_ISLINK(dir_ATTRS(d))
+#endif
+
+
 
 	#endif // _DIR_INTERNAL_H__
