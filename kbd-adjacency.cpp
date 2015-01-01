279d1d910562d57025b3bf2255a6f5b1bd5b5aed
Roman Senn <roman.l.senn@gmail.com>
2015-01-01 18:50:24 +0100

    ...

--- kbd-adjacency.cpp
+++ kbd-adjacency.cpp
@@ -38,8 +38,8 @@ range_to_v(std::initializer_list<T> il) {
 //-----------------------------------------------------------------------------
 template<class Char, class Container>
 typename std::enable_if< 
-  std::is_same< Char, typename Container::value_type >::value,
-	std::basic_ostream<Char>&
+std::is_same< Char, typename Container::value_type >::value,
+std::basic_ostream<Char>&
 >::type
 operator<<(std::basic_ostream<Char>& os, const Container& vc) {
 	std::copy(vc.begin(), vc.end(),  std::ostream_iterator<Char>(std::cout, ""));
@@ -63,7 +63,7 @@ collect_chars(const std::map<Char,std::vector<Char> >& m) {
 	for(auto& kv : m) {
 		r.insert(kv.first);
 		for(auto& ch : kv.second) {
-		  r.insert(ch);
+			r.insert(ch);
 		}
 	}
 	/*for(auto& kv : m) {
@@ -76,21 +76,21 @@ collect_chars(const std::map<Char,std::vector<Char> >& m) {
 //-----------------------------------------------------------------------------
 static adjacency_t
 make_adjacency_matrix() {
-  adjacency_t m;
+	adjacency_t m;
 
-  m['1'] = m['+'] = range_to_v("§°2\"qQ");
-  m['2'] = m['"'] = range_to_v("1+3*wWqQ");
-  m['3'] = m['*'] = range_to_v("2\"4çeE¿wW");
-  m['4'] = m['ç'] = range_to_v("3*5%rReE¿");
-  m['5'] = m['%'] = range_to_v("4ç6&tTrR");
-  m['6'] = m['&'] = range_to_v("5%7/zZtT");
-  m['7'] = m['/'] = range_to_v("6&8(uUzZ");
-  m['8'] = m['('] = range_to_v("7/9)iIuU");
-  m['9'] = m[')'] = range_to_v("8(0=oOiI");
-  m['0'] = m['='] = range_to_v("9)'?pPoO");
-  m['\''] = m['?'] = range_to_v("0=üèpP");
-  m['^'] = m['`'] = range_to_v("'?üè");
-  //m['"'] = m['!'] = range_to_v("'üè$£äà");
+	m['1'] = m['+'] = range_to_v("§°2\"qQ");
+	m['2'] = m['"'] = range_to_v("1+3*wWqQ");
+	m['3'] = m['*'] = range_to_v("2\"4çeE¿wW");
+	m['4'] = m['ç'] = range_to_v("3*5%rReE¿");
+	m['5'] = m['%'] = range_to_v("4ç6&tTrR");
+	m['6'] = m['&'] = range_to_v("5%7/zZtT");
+	m['7'] = m['/'] = range_to_v("6&8(uUzZ");
+	m['8'] = m['('] = range_to_v("7/9)iIuU");
+	m['9'] = m[')'] = range_to_v("8(0=oOiI");
+	m['0'] = m['='] = range_to_v("9)'?pPoO");
+	m['\''] = m['?'] = range_to_v("0=üèpP");
+	m['^'] = m['`'] = range_to_v("'?üè");
+	//m['"'] = m['!'] = range_to_v("'üè$£äà");
 
 	m['q'] = m['Q'] = range_to_v( {'1', '2', 'w', 'W', 'A', 'a'} );
 	m['w'] = m['W'] = range_to_v( {'q', 'Q', '2', '3', 'e', 'E', 's', 'S', 'a', 'A'} );
@@ -104,7 +104,7 @@ make_adjacency_matrix() {
 	m['p'] = m['P'] = range_to_v("oO0'üèöélL");
 	m['ü'] = m['è'] = range_to_v("pP'äàöé");
 
-//	m['a'] = m['A'] = range_to_v("qQwWsSyY<>");
+	//	m['a'] = m['A'] = range_to_v("qQwWsSyY<>");
 	m['a'] = m['A'] = range_to_v("qQwWsSyY");
 	m['s'] = m['S'] = range_to_v("aAwWeEdDxXyY");
 	m['d'] = m['D'] = range_to_v("sSeErRfFcCxX");
@@ -119,15 +119,15 @@ make_adjacency_matrix() {
 	m['$'] = m['£'] = range_to_v("äà");
 	m['<'] = m['>'] = range_to_v("aAyY");
 	m['y'] = m['Y'] = range_to_v("<>aAsSxX");
-  m['x'] = m['X'] = range_to_v("yYsSdDcC ");
-  m['c'] = m['C'] = range_to_v("xXdDfFvV ");
-  m['v'] = m['V'] = range_to_v("cCfFgGbB ");
-  m['b'] = m['B'] = range_to_v("vVgGhHnN ");
-  m['n'] = m['N'] = range_to_v("bBhHjJmM ");
-  m['m'] = m['M'] = range_to_v("nNjJkK,; ");
-  m[','] = m[';'] = range_to_v("mMkKlL.: ");
-  m['.'] = m[':'] = range_to_v(",;lLöé-_");
-  m['-'] = m['_'] = range_to_v(".:öéäà");
+	m['x'] = m['X'] = range_to_v("yYsSdDcC ");
+	m['c'] = m['C'] = range_to_v("xXdDfFvV ");
+	m['v'] = m['V'] = range_to_v("cCfFgGbB ");
+	m['b'] = m['B'] = range_to_v("vVgGhHnN ");
+	m['n'] = m['N'] = range_to_v("bBhHjJmM ");
+	m['m'] = m['M'] = range_to_v("nNjJkK,; ");
+	m[','] = m[';'] = range_to_v("mMkKlL.: ");
+	m['.'] = m[':'] = range_to_v(",;lLöé-_");
+	m['-'] = m['_'] = range_to_v(".:öéäà");
 
 	return m;
 };
@@ -138,7 +138,7 @@ template<typename CharT>
 std::function<bool(CharT)>
 make_predicate(const std::ctype<CharT>& ct, std::ctype_base::mask m) {
 	return std::bind(
-			(bool(std::ctype<CharT>::*)(std::ctype_base::mask,CharT) const) &std::ctype<CharT>::is, &ct, m, _1);
+		(bool(std::ctype<CharT>::*)(std::ctype_base::mask,CharT) const) &std::ctype<CharT>::is, &ct, m, _1);
 }
 
 int main() {
@@ -149,23 +149,23 @@ int main() {
 	using std::endl;
 	using std::ostream_iterator;
 
-  adjacency_t m = make_adjacency_matrix();
-	
+	adjacency_t m = make_adjacency_matrix();
+
 	std::locale lc("de_CH.iso88591"); 
 	const ctype<char>& ct = std::use_facet< ctype<char> >(lc); 
 
 	dump_map(m);
 
-  auto cl = collect_chars(m);
+	auto cl = collect_chars(m);
 
 	for(auto mask : {
-			ctype<char>::lower, 
-			ctype<char>::upper,
-			ctype<char>::digit,
-			ctype<char>::punct,
-			ctype<char>::graph,
-			ctype<char>::print
-			})
+		ctype<char>::lower,
+		ctype<char>::upper,
+		ctype<char>::digit,
+		ctype<char>::punct,
+		ctype<char>::graph,
+		ctype<char>::print
+	})
 	{
 		copy_if(cl.begin(), cl.end(), ostream_iterator<char>(cout), make_predicate(ct, mask));
 		cout << endl;
