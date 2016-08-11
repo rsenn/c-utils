7c609caa2fc794da481583eeef735e1dadb17ad8
Roman Senn <roman.l.senn@gmail.com>
2016-08-11 07:47:37 +0200

    ...

--- piccfghex.cpp
+++ piccfghex.cpp
@@ -1,5 +1,6 @@
 #include "intelhex.h"
 #include <cstdio>
+#include <cstring>
 #include <libgen.h>
 
 
@@ -21,9 +22,13 @@ int main(int argc, char* argv[])
   if (argc > 1) {
     std::string file = argv[1];
     std::string filename = basename(argv[1]);
-    int newval = 0xFF72;
-    if(argc > 2)
-      newval = std::stoul(argv[2], nullptr, 16);
+    int newval = -1; //0xFF72;
+    if(argc > 2) {
+      const char* p = argv[2];
+      if(!strncmp(p, "0x", 2)) p += 2;
+      newval = std::stol(p, nullptr, 16);
+printf("newval: %04X\n", newval);
+    }
 
     hex.load(file);
     printf("Highest address: 0x%04X\n", hex.max_address());
@@ -33,10 +38,13 @@ int main(int argc, char* argv[])
       printf("address: 0x%04X size: 0x%04X\n", p.first, p.second.size());
     }*/
     printf("old value: 0x%04X\n", config_reg);
-    printf("new value: 0x%04X\n", config_reg);
-    set16(hex, 0x400E, newval);
+    if(newval != -1) {
+	  set16(hex, 0x400E, newval);
+	  hex.set(0x4010, 0x3f);
+	  printf("new value: 0x%04X\n", newval);
+	  hex.write(filename.c_str());
+	}
 
-    hex.write((filename+".copy").c_str());
   }
 
   return 0;
