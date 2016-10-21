65b96dc86969dc5067f36f46472b76b0a66cc785
Roman Senn <roman.l.senn@gmail.com>
2016-10-21 04:31:17 +0200

    ...

--- lib/strlist_dump.c
+++ lib/strlist_dump.c
@@ -6,9 +6,23 @@ strlist_dump(buffer* out, const strlist* sl) {
   size_t n = strlist_count(sl);
   buffer_puts(out, "strlist[");
   buffer_putulong(out, n);
-  buffer_puts(out, "]{");
+  buffer_puts(out, "]{\"");
   for(size_t i = 0; i < n; ++i) {
-  	buffer_puts(out, strlist_at(sl, i));
+    const char* s = strlist_at(sl, i);
+    while(*s) {
+      char chrs[5] = { '\\', *s, '\0' };
+      char *p = &chrs[1];
+       if(!isprint(*p) || iscntrl(*p)) {
+        p[fmt_ulong(p, *p)] = '\0';
+        
+         --p;
+       }
+
+
+       buffer_puts(out, p); //, &chrs[sizeof(chrs) - 1]  - p);
+     
+       ++s;
+    }
   	if(i + 1 < n)
   		buffer_puts(out, "\", \"");
   }
