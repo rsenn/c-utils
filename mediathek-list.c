a9d34266421f8c52300ef0e87088791c2fef1c42
Roman Senn <roman.l.senn@gmail.com>
2017-09-18 15:13:06 +0200

    ...

--- mediathek-list.c
+++ mediathek-list.c
@@ -1,12 +1,17 @@
+
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <libgen.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include "buffer.h"
 #include "strlist.h"
 #include "str.h"
 
-const char* const mediathek_url = "http://download10.onlinetvrecorder.com/mediathekview/Filmliste-akt.xz";
+const char* argv0;
+
+const char* const mediathek_url = "http://verteiler1.mediathekview.de/Filmliste-akt.xz";
 
 static ssize_t
 buffer_dummyread(int fd, char* b, size_t n) { return 0; }
@@ -60,14 +65,24 @@ split_fields(strlist* sl, char* buf, size_t n)
   return ret;
 }
 
-int
-get_mediathek_list(const char* url)
+void process_status(void)
 {
-  int status;
+  /* display interesting process IDs  */
+  fprintf(stderr, "process %s: pid=%d, ppid=%d, pgid=%d, fg pgid=%dn\n",
+         argv0, (int)getpid(), (int)getppid(),
+         (int)getpgrp(), (int)tcgetpgrp(STDIN_FILENO));
+}
+
+int
+read_mediathek_list(const char* url)
+{
+/*  int status;
   int xzpid;
   int xzpipe[2];
   int clpipe[2];
   int clpid;
+
+  process_status();
 
   if(pipe(xzpipe) != 0) return -1;
   if(pipe(clpipe) != 0) return -1;
@@ -92,46 +107,59 @@ get_mediathek_list(const char* url)
     exit(1);
   }
 
-  {
-    char buf[1024];
-    char buf2[16384];
-    ssize_t ret, ret2;
-    strlist sl;
-    buffer b = BUFFER_INIT(read, xzpipe[0], buf, sizeof(buf));
-
-    strlist_init(&sl);
-
-    while((ret = buffer_get_token(&b, buf2, sizeof(buf2), "]", 1)) > 0) {
-
-      for(;;) {
-        buf2[ret++] = ']';
-        ret2 = buffer_get(&b, &buf2[ret], 1);
-        if(ret2 > 0) {
-
-          if(buf2[ret] == ',') break;
-
-          ret += ret2;
-          ret2 = buffer_get_token(&b, &buf2[ret], sizeof(buf2) - ret, "]", 1);
-          if(ret2 > 0)
-            ret += ret2;
-        }
-      }
-
-      split_fields(&sl, buf2, ret);
-    }
-
-    buffer_flush(buffer_1);
-  }
-
-  kill(xzpid, SIGKILL);
-  waitpid(xzpid, &status, WNOHANG);
-  kill(clpid, SIGKILL);
-  waitpid(clpid, &status, WNOHANG);
-  return 0;
+  return xzpipe[0];*/
+  
+  stralloc cmd;
+  stralloc_init(&cmd);
+  stralloc_copys(&cmd, "curl -s ");
+  stralloc_cats(&cmd, url);
+  stralloc_cats(&cmd, "| xzcat");
+  stralloc_0(&cmd);
+  
+  static FILE *pfd;
+  
+  if((pfd  = popen(cmd.s, "r")) == NULL) return -1;
+  
+  return fileno(pfd);
 }
 
-int main()
+
+int
+parse_mediathek_list(int fd)
 {
-  get_mediathek_list(mediathek_url);
+  char buf[1024];
+  char buf2[16384];
+  ssize_t ret, ret2;
+  strlist sl;
+  buffer b = BUFFER_INIT(read, fd, buf, sizeof(buf));
+
+  strlist_init(&sl);
+
+  while((ret = buffer_get_token(&b, buf2, sizeof(buf2), "]", 1)) > 0) {
+
+    for(;;) {
+      buf2[ret++] = ']';
+      ret2 = buffer_get(&b, &buf2[ret], 1);
+      if(ret2 > 0) {
+
+        if(buf2[ret] == ',') break;
+
+        ret += ret2;
+        ret2 = buffer_get_token(&b, &buf2[ret], sizeof(buf2) - ret, "]", 1);
+        if(ret2 > 0)
+          ret += ret2;
+      }
+    }
+
+    split_fields(&sl, buf2, ret);
+  }
+
+  buffer_flush(buffer_1);
+}
+
+int main(int argc, char *argv[])
+{
+  argv0 = basename(argv[0]);
+  return parse_mediathek_list(read_mediathek_list(mediathek_url));
 }
 
