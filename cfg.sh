ef31aa9438b407e7bfb4df19bebadc3f964cf29e
Roman Senn <roman.l.senn@gmail.com>
2017-09-18 23:51:07 +0200

    ...

---
+++ cfg.sh
@@ -0,0 +1,20 @@
+cfg () 
+{ 
+    case $(uname -o) in
+        MSys|MSYS|Msys) SYSTEM="MSYS" ;;
+        *) SYSTEM="Unix" ;;
+    esac
+
+
+    : ${builddir=build/cmake}
+
+    ( mkdir -p $builddir;
+    cd $builddir;
+    set -x
+    cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=$(get-prefix) \
+    -G "${SYSTEM:-MSYS} Makefiles" \
+    -DCMAKE_VERBOSE_MAKEFILE=TRUE \
+    -DCMAKE_BUILD_TYPE="${TYPE:-RelWithDebInfo}" \
+    "$@" \
+     ../.. )
+}
