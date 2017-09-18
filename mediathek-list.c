96bc1887f1d4f53b4fd8a39a55db9bf1a0b014e6
Roman Senn <roman.l.senn@gmail.com>
2017-09-18 16:46:14 +0200

    ...

--- mediathek-list.c
+++ mediathek-list.c
@@ -3,11 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libgen.h>
+#include <time.h>
+#include <sys/time.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include "buffer.h"
 #include "strlist.h"
 #include "str.h"
+#include "scan.h"
+#include "fmt.h"
 
 const char* argv0;
 
@@ -30,7 +34,7 @@ count_field_lengths(strlist* sl, int lengths[21])
 }
 
 int
-split_fields(strlist* sl, char* buf, size_t n)
+split_fields(strlist* sl, strlist* prev, char* buf, size_t n)
 {
   int ret = 0;
   char buf2[4096];
@@ -57,11 +61,14 @@ split_fields(strlist* sl, char* buf, size_t n)
       buf2[n++] = *p++;
     }
 
-    strlist_pushb(sl, buf2, n);
+    if(ret >= 1 && ret <= 2 && n == 0) {
+      strlist_push(sl, strlist_at(prev, ret));
+    } else {
+      strlist_pushb(sl, buf2, n);
+    }
     ret++;
   }
 
-  strlist_dump(buffer_2, sl);
   return ret;
 }
 
@@ -69,71 +76,173 @@ void process_status(void)
 {
   /* display interesting process IDs  */
   fprintf(stderr, "process %s: pid=%d, ppid=%d, pgid=%d, fg pgid=%dn\n",
-         argv0, (int)getpid(), (int)getppid(),
-         (int)getpgrp(), (int)tcgetpgrp(STDIN_FILENO));
+          argv0, (int)getpid(), (int)getppid(),
+          (int)getpgrp(), (int)tcgetpgrp(STDIN_FILENO));
 }
 
 int
 read_mediathek_list(const char* url)
 {
-/*  int status;
-  int xzpid;
-  int xzpipe[2];
-  int clpipe[2];
-  int clpid;
+  /*  int status;
+    int xzpid;
+    int xzpipe[2];
+    int clpipe[2];
+    int clpid;
 
-  process_status();
+    process_status();
 
-  if(pipe(xzpipe) != 0) return -1;
-  if(pipe(clpipe) != 0) return -1;
+    if(pipe(xzpipe) != 0) return -1;
+    if(pipe(clpipe) != 0) return -1;
 
-  if((xzpid = fork()) == 0) {
+    if((xzpid = fork()) == 0) {
 
-    close(STDOUT_FILENO);
-    dup(xzpipe[1]);
-    close(STDIN_FILENO);
-    dup(clpipe[0]);
+      close(STDOUT_FILENO);
+      dup(xzpipe[1]);
+      close(STDIN_FILENO);
+      dup(clpipe[0]);
 
-    execlp("xzcat", "xzcat", NULL);
-    exit(1);
-  }
+      execlp("xzcat", "xzcat", NULL);
+      exit(1);
+    }
 
-  if((clpid = fork()) == 0) {
+    if((clpid = fork()) == 0) {
 
-    close(STDOUT_FILENO);
-    dup(clpipe[1]);
+      close(STDOUT_FILENO);
+      dup(clpipe[1]);
 
-    execlp("curl", "curl", "-s", url, NULL);
-    exit(1);
-  }
+      execlp("curl", "curl", "-s", url, NULL);
+      exit(1);
+    }
 
-  return xzpipe[0];*/
-  
+    return xzpipe[0];*/
+
   stralloc cmd;
   stralloc_init(&cmd);
   stralloc_copys(&cmd, "curl -s ");
   stralloc_cats(&cmd, url);
   stralloc_cats(&cmd, "| xzcat");
   stralloc_0(&cmd);
-  
+
   static FILE *pfd;
-  
+
   if((pfd  = popen(cmd.s, "r")) == NULL) return -1;
-  
+
   return fileno(pfd);
 }
 
+
+unsigned long
+parse_time(const char* s) { 
+  size_t r = 0;
+  unsigned short n;
+  size_t l;
+  
+  if(s == NULL) return 0;
+  
+  for(;;) {  
+    l = scan_ushort(s, &n);
+    if(l == 0) break;
+    r += n;
+    s += l;
+    if(*s == ':') {
+      r *= 60;
+      ++s;
+      continue;
+    }
+    if(*s == '\0') break;
+  }
+  return r;  
+}
+
+char*
+format_num(time_t num) {
+  static char buf[1024];
+  memset(buf, 0,  sizeof(buf));
+  buf[fmt_ulonglong(buf, num)] = '\0';
+  return buf;
+}
+
+char*
+format_time(time_t ti) {
+
+  static char buf[1024];
+  
+  unsigned short h, m, s;
+  size_t i = 0;
+  
+  s = ti % 60;
+  ti /= 60;
+  m = ti % 60;
+  ti /= 60;
+  h = ti;
+  
+  if(h < 10) buf[i++] = '0';
+  i += fmt_ulong(&buf[i], h);
+  buf[i++] = ':';
+  if(m < 10) buf[i++] = '0';
+  i += fmt_ulong(&buf[i], m);
+  buf[i++] = ':';
+  if(s < 10) buf[i++] = '0';
+  i += fmt_ulong(&buf[i], s);
+  buf[i] = '\0';
+  
+  return buf;
+}
+
+
+time_t
+parse_datetime(const char* s, const char* fmt) {
+  struct tm tm_s;
+  memset(&tm_s, 0, sizeof(struct tm));
+  if(strptime(s, fmt, &tm_s) == s)  
+    return 0;
+  return mktime(&tm_s);
+}
+
+char*
+format_datetime(size_t t, const char* fmt) {
+  static char buf[1024]; 
+  time_t tm = t;
+   size_t n = strftime(buf, sizeof(buf), fmt, localtime(&tm));
+   
+ return buf; //  buffer_put(b, buf, n);
+}
+
+void
+output_entry(buffer* b, strlist* sl) {
+
+  const char* sep = ", ";
+  time_t dt = parse_datetime(strlist_at(sl, 4), "%d.%m.%Y");
+  
+  time_t tm = parse_time(strlist_at(sl, 5));
+  time_t dr = parse_time(strlist_at(sl, 6));  /* duration */
+  
+  size_t mbytes = 0; 
+  scan_uint(strlist_at(sl, 7), &mbytes);
+  
+//  fprintf(stderr, "datetime: %llu\n", datetime);
+  //buffer_putm(b, "time: ",  format_time(ti), " (", format_num(datetime), ") ", sep, NULL);
+
+  buffer_putm(b, "Sender: ", strlist_at(sl, 1), sep, NULL);
+  buffer_putm(b, "Thema: ", strlist_at(sl, 2), sep, NULL);
+  buffer_putm(b, "Titel: ", strlist_at(sl, 3), sep, NULL); 
+  
+  buffer_putm(b, "Datum/Zeit: ", format_datetime(dt+tm, "%d.%m.%Y %H:%M:%S"), sep, NULL);
+  buffer_putm(b, "Dauer: ", format_time(dr), sep, NULL);
+   
+  buffer_putnlflush(b);
+}
 
 int
 parse_mediathek_list(int fd)
 {
   char buf[1024];
-  char buf2[16384];
+  char buf2[32768];
   ssize_t ret, ret2;
-  strlist sl;
+  strlist prev, sl;
   buffer b = BUFFER_INIT(read, fd, buf, sizeof(buf));
 
-  strlist_init(&sl);
+  strlist_init(&prev);
 
   while((ret = buffer_get_token(&b, buf2, sizeof(buf2), "]", 1)) > 0) {
 
@@ -151,11 +260,23 @@ parse_mediathek_list(int fd)
       }
     }
 
-    split_fields(&sl, buf2, ret);
+    strlist_init(&sl);
+    split_fields(&sl, &prev, buf2, ret);
+
+  
+    strlist_dump(buffer_2, &sl);
+    output_entry(buffer_1, &sl);
+
+    prev = sl;
   }
 
   buffer_flush(buffer_1);
 }
+
+//kill(xzpid, SIGKILL);
+//waitpid(xzpid, &status, WNOHANG);
+//kill(clpid, SIGKILL);
+//waitpid(clpid, &status, WNOHANG);
 
 int main(int argc, char *argv[])
 {
