c90ed29e66c3e19a26b0c1ff83d716253327fe9c
Roman Senn <roman.l.senn@gmail.com>
2017-09-29 01:18:49 +0200

    ...

--- mediathek-list.c
+++ mediathek-list.c
@@ -7,7 +7,9 @@
 #include <sys/time.h>
 #include <sys/wait.h>
 #include <signal.h>
+
 #include "buffer.h"
+#include "byte.h"
 #include "strlist.h"
 #include "str.h"
 #include "scan.h"
@@ -244,6 +246,56 @@ make_url(const char* base, const char* trail) {
   return url.s;
 }
 
+typedef struct mediathek_entry {
+  stralloc channel;
+  stralloc topic;
+  stralloc title;
+  time_t tm;
+  time_t dr;
+  unsigned int mbytes;
+  stralloc desc;
+  stralloc url;
+  stralloc link;
+} mediathek_entry_t;
+
+mediathek_entry_t*
+new_mediathek_entry() {
+   mediathek_entry_t* e = malloc(sizeof(mediathek_entry_t));
+   if(e == NULL) return NULL;
+   byte_zero(e, sizeof(mediathek_entry_t));
+   return e;
+}
+
+mediathek_entry_t*
+create_mediathek_entry(const char* ch, const char* tpc, const char* tit, const char* dsc, const char* ur, const char* ln) {
+   mediathek_entry_t* e = new_mediathek_entry();
+   if(e == NULL) return NULL;
+
+   stralloc_copys(&e->channel, ch); stralloc_0(&e->channel);
+   stralloc_copys(&e->topic, tpc); stralloc_0(&e->topic);
+   stralloc_copys(&e->title, tit); stralloc_0(&e->title);
+
+   stralloc_copys(&e->desc, dsc); stralloc_0(&e->desc);
+   stralloc_copys(&e->url, ur); stralloc_0(&e->url);
+   stralloc_copys(&e->link, ln); stralloc_0(&e->link);
+   
+   return e;
+}
+
+void
+delete_mediathek_entry(mediathek_entry_t* e) {
+  stralloc_free(&e->channel);
+  stralloc_free(&e->topic);
+  stralloc_free(&e->title);
+  
+  stralloc_free(&e->desc);
+  stralloc_free(&e->url);
+  stralloc_free(&e->link);
+  free(e);
+}
+
+static mediathek_entry_t* e;
+
 int
 parse_entry(buffer* b, strlist* sl) {
 
@@ -266,13 +318,24 @@ parse_entry(buffer* b, strlist* sl) {
   const char* url = strlist_at(sl, 9);
   const char* link = strlist_at(sl, 10);
 
-  buffer_putm(b, "Kanal:\t", strlist_at(sl, 1), sep, NULL);
-  buffer_putm(b, "Thema:\t", strlist_at(sl, 2), sep, NULL);
-  buffer_putm(b, "Titel:\t", strlist_at(sl, 3), sep, NULL);
+/*  mediathek_entry_t* */e = create_mediathek_entry(
+      strlist_at(sl, 1), strlist_at(sl, 2), strlist_at(sl, 3),
 
-  buffer_putm(b, "Datum:\t", format_datetime(dt + tm, "%Y%m%d %H:%M"), sep, NULL);
-  buffer_putm(b, "Dauer:\t", format_time(dr), sep, NULL);
-  buffer_putm(b, "Grösse:\t", format_num(mbytes), "MB", sep, NULL);
+      desc, url, link
+      
+  );
+  
+  e->tm = dt + tm;
+  e->dr =  dr;
+  e->mbytes = mbytes;
+
+  buffer_putm(b, "Kanal:\t", e->channel /*strlist_at(sl, 1)*/, sep, NULL);
+  buffer_putm(b, "Thema:\t", e->topic /*strlist_at(sl, 2)*/, sep, NULL);
+  buffer_putm(b, "Titel:\t", e->title /*strlist_at(sl, 3)*/, sep, NULL);
+
+  buffer_putm(b, "Datum:\t", format_datetime(e->tm, "%Y%m%d %H:%M"), sep, NULL);
+  buffer_putm(b, "Dauer:\t", format_time(e->dr), sep, NULL);
+  buffer_putm(b, "Grösse:\t", format_num(e->mbytes), "MB", sep, NULL);
 
  /* buffer_putm(b, "URL:\t", url , sep, NULL);
   buffer_putm(b, "URL lo:\t", make_url(url, strlist_at(sl, 13)), sep, NULL);
@@ -347,6 +410,9 @@ parse_mediathek_list(int fd) {
     if(parse_entry(buffer_2, &sl) == 0) {
       if(strlist_count(&prev)) buffer_put(buffer_1, ",\n", 2);
       output_entry(buffer_1, &sl);
+      
+      delete_mediathek_entry(e);
+      e = NULL;
     }
 
     prev = sl;
