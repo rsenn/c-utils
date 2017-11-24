5fc1d7a26666db2187227bc57ce8e778b95d57b0
Roman Senn <roman.l.senn@gmail.com>
2017-11-24 14:35:15 +0100

    ...

--- cfg.sh
+++ cfg.sh
@@ -6,12 +6,13 @@ cfg ()
     esac
 
 
+    : ${prefix=/usr}
     : ${builddir=build/cmake}
 
     ( mkdir -p $builddir;
     cd $builddir;
     set -x
-    ${CMAKE-cmake} -Wno-dev -DCMAKE_INSTALL_PREFIX=$(get-prefix) \
+    ${CMAKE-cmake} -Wno-dev -DCMAKE_INSTALL_PREFIX="$prefix" \
     -G "${SYSTEM:-MSYS} Makefiles" \
     -DCMAKE_VERBOSE_MAKEFILE=TRUE \
     -DCMAKE_BUILD_TYPE="${TYPE:-RelWithDebInfo}" \
