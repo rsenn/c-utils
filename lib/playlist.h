1b8399a514254b03c2d9cd597140332ab28f7d0e
Roman Senn <you@example.com>
2017-11-30 13:53:01 +0100

    ...

--- lib/playlist.h
+++ lib/playlist.h
@@ -41,6 +41,7 @@ typedef struct playlist {
 void playlist_init(playlist *pl);
 void playlist_xspf(playlist *pls, buffer *b);
 void playlist_m3u(playlist *pls, buffer *b);
+void playlist_pls(playlist *pls, buffer *b);
 
 int playlist_read(playlist *pl);
 
