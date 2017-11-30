d373ab2750167a2e202b8f404a696e7a0f0aac3f
Roman Senn <you@example.com>
2017-11-30 11:33:50 +0100

    ...

---
+++ lib/playlist_init.c
@@ -0,0 +1,7 @@
+#include "playlist.h"
+#include "byte.h"
+
+void
+playlist_init(playlist* pl) {
+  byte_zero(pl, sizeof(playlist));
+}
---
+++ lib/playlist_xspf.c
@@ -0,0 +1,16 @@
+#include <libxml/xmlreader.h>
+#include "playlist.h"
+#include "buffer.h"
+
+int
+buffer_close_return_int(buffer *b) {
+  buffer_close(b);
+  return 0;
+}
+
+void
+playlist_xspf(playlist* pls, buffer *b) {
+  xmlTextReaderPtr rd;
+  rd = xmlReaderForIO((xmlInputReadCallback)buffer_get, (xmlInputCloseCallback)buffer_close_return_int, b, "", "UTF-8", 0);
+  pls->ptr = rd;
+}
