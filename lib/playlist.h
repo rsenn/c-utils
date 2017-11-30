0295c036dc84db9c7620fcc3f5602d7f7706172e
Roman Senn <you@example.com>
2017-11-30 13:23:26 +0100

    ...

--- lib/playlist.h
+++ lib/playlist.h
@@ -14,7 +14,7 @@ extern "C" {
 
 struct playlist;
 
-typedef int (playlist_callback)(struct playlist*, stralloc* title, stralloc* path, uint32 len);
+typedef void (playlist_callback)(struct playlist*, stralloc* title, stralloc* path, uint32 len);
 typedef playlist_callback* playlist_callback_ptr;
 
 typedef enum playlist_type {
