4d0454934db940d869971e66b4eb2d78c7ce7db3
Roman Senn <you@example.com>
2018-01-09 10:40:49 +0100

    ...

--- plstest.c
+++ plstest.c
@@ -2,12 +2,16 @@
 #include "open.h"
 #include "buffer.h"
 #include "byte.h"
+#include "str.h"
 #include <unistd.h>
 
   playlist pls1, pls2;
   static buffer outfile;
   static char outbuf[1024];
   
+static  buffer inbuf;
+
+
 static void
 playlist_process(playlist* pl, stralloc* title, stralloc* location, uint32 length) {
   buffer_putm(buffer_2, "Title: ", title->s, "\n", NULL);
@@ -26,9 +30,24 @@ e.length = length;
  playlist_write_entry(&outfile, &pls1, &e); 
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 
-  buffer inbuf;
+int opt;
+playlist_type type = M3U;
+ while((opt = getopt(argc, argv, "t:")) != -1) {
+    switch(opt) {
+    case 't':
+      if(!str_diff(optarg, "pls")) type = PLS;
+      else if(!str_diff(optarg, "m3u")) type = M3U;
+      else if(!str_diff(optarg, "xspf"))  type = XSPF;
+      break;
+    default: /* '?' */
+      buffer_putm(buffer_2, "Usage: ", argv[0], "[-t TYPE] [file]\n");
+      exit(EXIT_FAILURE);
+    } 
+  } 
+
+
 
 
   playlist_init(&pls1);
@@ -42,7 +61,7 @@ int main() {
   */
   
   
-  playlist_settype(&pls1, PLS);
+  playlist_settype(&pls1, type);
 
 buffer_init(&outfile, write, open_trunc("playlist.out"), outbuf, sizeof(outbuf));
   
