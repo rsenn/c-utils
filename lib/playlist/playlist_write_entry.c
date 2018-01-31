1ec1baea93d8bb69ad1d28b7d9fd087214ba00bf
Roman Senn <you@example.com>
2018-02-01 00:29:37 +0100

    ...

--- lib/playlist/playlist_write_entry.c
+++ lib/playlist/playlist_write_entry.c
@@ -2,49 +2,50 @@
 
 int
 playlist_write_entry(buffer* b, playlist* pl, playlist_entry* e) {
+  stralloc* title =  e->title.len ? &e->title : &e->path;
   ++pl->count;
   switch(pl->type) {
-  case M3U: {
-    buffer_puts(b, "#EXTINF:");
-    buffer_putulong(b, e->length);
-    buffer_puts(b, ",");
-    buffer_putsa(b, e->title.len ? &e->title : &e->path);
-    buffer_puts(b, "\n");
-    buffer_putsa(b, &e->path);
-    buffer_puts(b, "\n");
-    break;
-  }
-  case PLS: {
-    buffer_puts(b, "File");
-    buffer_putulong(b, pl->count);
-    buffer_puts(b, "=");
-    buffer_putsa(b, &e->path);
-    buffer_puts(b, "\nTitle");
-    buffer_putulong(b, pl->count);
-    buffer_puts(b, "=");
-    buffer_putsa(b, &e->title);
-    buffer_puts(b, "\nLength");
-    buffer_putulong(b, pl->count);
-    buffer_puts(b, "=");
-    buffer_putulong(b, e->length);
-    buffer_puts(b, "\n");
-    break;
-  }
-  case XSPF: {
+    case M3U: {
+      buffer_puts(b, "#EXTINF:");
+      buffer_putulong(b, e->length);
+      buffer_puts(b, ",");
+      buffer_putsa(b, title);
+      buffer_puts(b, "\n");
+      buffer_putsa(b, &e->path);
+      buffer_puts(b, "\n");
+      break;
+    }
+    case PLS: {
+      buffer_puts(b, "File");
+      buffer_putulong(b, pl->count);
+      buffer_puts(b, "=");
+      buffer_putsa(b, &e->path);
+      buffer_puts(b, "\nTitle");
+      buffer_putulong(b, pl->count);
+      buffer_puts(b, "=");
+      buffer_putsa(b, title);
+      buffer_puts(b, "\nLength");
+      buffer_putulong(b, pl->count);
+      buffer_puts(b, "=");
+      buffer_putulong(b, e->length);
+      buffer_puts(b, "\n");
+      break;
+    }
+    case XSPF: {
 
-    buffer_puts(b, "    <track>\n      <location>");
-    buffer_putsa(b, &e->path);
-    buffer_puts(b, "</location>\n      <title>");
-    buffer_putsa(b, &e->title);
-    buffer_puts(b, "</title>\n         <duration>");
-    buffer_putulong(b, e->length);
-    buffer_puts(b, "</duration></track>\n");
-    break;
-  }
-  case UNKNOWN:
-  default: {
-    return 0;
-  }
+      buffer_puts(b, "    <track>\n      <location>");
+      buffer_putsa(b, &e->path);
+      buffer_puts(b, "</location>\n      <title>");
+      buffer_putsa(b, title);
+      buffer_puts(b, "</title>\n      <duration>");
+      buffer_putulong(b, e->length);
+      buffer_puts(b, "</duration>\n    </track>\n");
+      break;
+    }
+    case UNKNOWN:
+    default: {
+      return 0;
+    }
   }
   return 1;
 }
