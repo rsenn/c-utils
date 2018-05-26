69673355a6a8c93109e7558d36a5c3b28c1cb44f
Roman Senn <roman.l.senn@gmail.com>
2018-05-26 15:46:06 +0200

    ...

--- lib/dir/dir_time.c
+++ lib/dir/dir_time.c
@@ -9,12 +9,18 @@
 #include "dir.h"
 #include "dir_internal.h"
 
+#if _FILE_OFFSET_BITS == 64
+typedef struct stat stat_t;
+#else
+typedef struct stat stat_t;
+#endif
+
 time_t
 dir_time(struct dir_s* d, int time_type) {
   unsigned long long r = 0;
 #if USE_READDIR
   const char* name = dir_INTERNAL(d)->dir_entry->d_name;
-  struct stat st;
+  stat_t st;
 
   lstat(name, &st);
 
