94211b1fb533e03f15294c1a7010454257ca3b78
Roman Senn <roman.l.senn@gmail.com>
2018-07-20 14:28:17 +0200

    ...

---
+++ lib/buffer/buffer_skip_pred.c
@@ -0,0 +1,15 @@
+#include "../buffer.h"
+
+int
+buffer_skip_pred(buffer* b, int (*pred)(char)) {
+  char c;
+  int n = 0;
+  for(;;) {
+   if(buffer_peekc(b, &c) < 0) return -1;
+   if(!pred(c)) break;
+   buffer_skipc(b); 
+   ++n; 
+  }
+  return n;
+}
+
