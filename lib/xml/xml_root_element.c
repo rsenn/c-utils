61fcac62e0716f702bc285b4cb0828c3799f0410
Roman Senn <roman.l.senn@gmail.com>
2018-07-28 02:45:44 +0200

    ...

---
+++ lib/xml/xml_root_element.c
@@ -0,0 +1,14 @@
+#include "../xml.h"
+#include <ctype.h>
+
+xmlnode*
+xml_root_element(xmlnode* node) {
+  for(node = node->children; node; node = node->next) {
+    if(node->type == XML_ELEMENT && isalnum(node->name[0])) {
+      if(node->children)
+        return node;
+    }
+  }
+  return NULL;
+}
+
