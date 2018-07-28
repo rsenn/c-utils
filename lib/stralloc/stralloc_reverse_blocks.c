3f30a9f0aacf7cfaa1b46bc7084fde1415af89ee
Roman Senn <roman.l.senn@gmail.com>
2018-07-28 06:58:31 +0200

    ...

--- lib/stralloc/stralloc_reverse_blocks.c
+++ lib/stralloc/stralloc_reverse_blocks.c
@@ -6,10 +6,21 @@
 void stralloc_reverse_blocks(stralloc *sa, size_t size) {
   size_t n = sa->len / (size << 1) ;
   size_t i = 0 ;
+
+#ifdef __GNUC__
+  char tmp[size];
+#elif defined(HAVE_ALLOCA)
   char* tmp = alloca(size);
+#else
+  char* tmp = malloc(size);
+#endif
   for(; i < n ; i++) {
     byte_copy(tmp, size, sa->s + i * size) ;
     byte_copy(sa->s + i * size, size, sa->s + (2 * n - 1 - i) * size) ;
     byte_copy(sa->s + (2 * n - 1 - i) * size, size, tmp) ;
   }
+
+#if !defined(__GNUC__) && !defined(HAVE_ALLOCA)
+  free(tmp);
+#endif
 }
