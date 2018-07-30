a26aac95707626b8d89d7e5c8511fa7d00c8bcb0
Roman Senn <roman.l.senn@gmail.com>
2018-07-30 13:56:27 +0200

    ...

--- lib/buffer/buffer_putnspace.c
+++ lib/buffer/buffer_putnspace.c
@@ -14,20 +14,21 @@
 
 int
 buffer_putnspace(buffer* b, int n) {
-  if(n > 0) {
+  if(n <= 0) return 0;
+
+  {
     int ret;
     char* space =
 #ifdef HAVE_ALLOCA_FUNC
-        alloca(n);
+      alloca(n);
 #else
-        malloc(n);
+      malloc(n);
 #endif
     byte_fill(space, n, ' ');
     ret = buffer_put(b, space, n);
 #ifndef HAVE_ALLOCA_FUNC
     free(space);
 #endif
-    return n;
+    return ret;
   }
-  return 0;
 }
