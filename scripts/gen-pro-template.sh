94fd2c201862be128d285328f19ba2fabcade51d
Roman Senn <roman.l.senn@gmail.com>
2018-07-30 17:17:56 +0200

    ...

--- scripts/gen-pro-template.sh
+++ scripts/gen-pro-template.sh
@@ -1,13 +1,17 @@
-gen-pro-template () 
-{ 
+gen-pro-template() { 
+ (IFS="
+ "
+  SOURCE=${1##*/}.c
 
-  SOURCE=${1%.*}.c
-
-  SOURCES=$(scripts/get-sources.sh build/*/Debug/"${1%.*}")
+  SOURCES=$(echo $(scripts/get-sources.sh build/*/Debug/"${SOURCE%.*}" ) | sed 's| [^ ]\+\.h||g')
 
   : ${SOURCES:="$SOURCE"}
-  HEADERS=$(for x in $(sed -n '/include/ { s,.*"\([^"]*\)".*,\1, ; s,lib/,, ; p }' $SOURCES); do
-  find * -iname "$x"; done )
+  HEADERS=$(for x in $(sed -n '/include/ { s,.*"\([^"]*\)".*,\1, ; s,lib/,, ; p }' $SOURCES | sort -u); do
+  case "$x" in
+    '#include'*|*[\<\>]*) continue ;;
+  esac
+  find . -iname "${x##*/}"; done )
+  HEADERS=$(echo $HEADERS |sed 's,\s\+\./, ,g ; s|\sbuild/[^ ]*||g')
 
   PROJECT=${SOURCE%.*}.pro
 
@@ -21,11 +25,12 @@ TARGET = ${SOURCE%.*}
 
 INCLUDEPATH += . \$\$PWD/lib
 
-HEADERS = ${SOURCES}
-SOURCES = ${HEADERS}
+HEADERS = $(echo ${HEADERS})
+SOURCES = $(echo ${SOURCES})
 
 include(deployment.pri)
 qtcAddDeployment()
 
 EOF
+)
 }
