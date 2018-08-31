1834abb71c975345668f479658e3cbe306031a2b
Roman Senn <roman.l.senn@gmail.com>
2018-08-31 21:47:09 +0200

    ...

--- scripts/get-a-deps.sh
+++ scripts/get-a-deps.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+pushv() {
+    eval "shift;$1=\"\${$1+\"\$$1\${IFS%\"\${IFS#?}\"}\"}\$*\""
+}
 isin() {
  (needle="$1";
   while [ "$#" -gt 1 ]; do
@@ -11,7 +15,17 @@ isin() {
 
 get_a_deps() {
 
+  while :; do
+    case "$1" in
+      --cmake | -c) CMAKE=true; shift ;;
+      --direct | -d) DIRECT=true; shift ;;
+      *) break ;;
+    esac
+  done
+
   DIR="$1"
+  shift
+  OBJ="$*"
     IFS=" ""
 :"
 
@@ -39,21 +53,31 @@ get_a_deps() {
 
   
   ${NM-nm} -A "$@"   |
-    sed -n "/ _/! s|.*/\([^ ]*\.a\):.* \([UT]\) \(.*\)|\1 \2 \3|p" >"$NMFILE"
+    sed -n "/ _/! { s|:[^ :]*\.o||;  s|.*/\([^ ]*\.[ao]\):.* \([UT]\) \(.*\)|\1 \2 \3|p }" >"$NMFILE"
 
+
+  unset DEPS
 
   get_dep() {
     EXCLUDE_EXPR=$(sed -n  "/^$(basename "$1") T / { s|.* \(.*\)|/ \1$/d|; p }" "$NMFILE" )
     MATCH_EXPR=$(sed -n  "$EXCLUDE_EXPR;  /^$(basename "$1") U / { s|.* \(.*\)|/\.a T \1/ { s/ .*//p } |; p }" "$NMFILE")
 
-    set -- "$(basename "$1")" $(sed -n "$MATCH_EXPR" "$NMFILE" |sort -u )
+    if ! isin "$1" $DEPS; then
+      echo "$INDENT$1" 1>&2 
+      pushv DEPS "$1"
+    fi
 
+    set -- $(sed -n "$MATCH_EXPR" "$NMFILE" |sort -u )
 
-    echo "${INDENT}$1"
-    shift
-    for DEP; do 
-      echo "${INDENT}  $DEP"
-    done
+      for DEP; do 
+        if ! isin "$DEP" $DEPS; then
+          pushv DEPS "$DEP"
+      #    echo "${INDENT}  $DEP"
+           if [ "$DIRECT" != true ]; then
+            INDENT="$INDENT  " get_dep "$DEP"
+          fi
+      fi
+      done
 
 #    for DEP; do 
 #      if ! isin "$DEP" $DEPS; then
@@ -61,6 +85,20 @@ get_a_deps() {
 #      fi
 #    done
   }
+
+  if [ -n "$OBJ" ]; then
+    for O in $OBJ; do 
+      unset DEPS
+      get_dep "$O"
+      set -- $DEPS
+      [ "$1" = "$O" ] && shift
+      if [ "$CMAKE" = true ]; then
+        [ $# -gt 0 ] && echo "target_link_libraries($(basename $O .o) ${@%.a})"
+      else
+        echo $OBJ: ${@%.a}
+      fi
+    done
+  fi
   
  
 }
@@ -69,4 +107,3 @@ case "${0##*/}" in
   -* | sh | bash) ;;
   *) get_a_deps "$@" || exit $? ;;
 esac
-
