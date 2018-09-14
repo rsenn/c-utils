4302e2eeb32a859858707b6f3cca0465e271d398
Roman Senn <roman.l.senn@gmail.com>
2018-09-14 09:56:05 +0200

    ...

---
+++ lib/path/path_exists.c
@@ -0,0 +1,16 @@
+#include "../windoze.h"
+#include "../path.h"
+
+#if !WINDOWS_NATIVE
+#include <sys/stat.h>
+#endif
+
+int
+path_exists(const char* p) {
+#if WINDOWS_NATIVE
+  return access(p, 0) == 0;
+#else
+  struct stat st;
+  return lstat(p, &st) == 0;
+#endif
+}
---
+++ lib/path/path_num_sa.c
@@ -0,0 +1,8 @@
+#include "../path.h"
+
+size_t
+path_num_sa(const char* p, size_t len, stralloc* sa, int n) {
+  size_t r = path_num(p, len, n);
+  stralloc_copyb(sa, p, r);
+  return r;
+}
