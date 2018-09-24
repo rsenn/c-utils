3a2fe562744cae01eddcd62f1298de8f9a6f1d8b
Roman Senn <roman.l.senn@gmail.com>
2018-09-24 06:05:06 +0200

    ...

---
+++ lib/path/path_absolute.c
@@ -0,0 +1,15 @@
+#include "../windoze.h"
+#include "../path.h"
+#include <ctype.h>
+
+int
+path_absolute(const char* p) {
+  size_t len = str_len(p);
+  if(len > 0 && p[0] == '/')
+    return 1;
+#if WINDOWS
+  if(len >= 2 && isalpha(p[0]) && p[1] == ':')
+    return 1;
+#endif
+  return 0;
+}
