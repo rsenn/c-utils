76b97d965818dfb4a36b35ba599ad38b99e00d00
Roman Senn <roman.l.senn@gmail.com>
2018-09-29 12:13:50 +0200

    ...

--- lib/pe/pe_filehdr_ptr.c
+++
@@ -1,9 +0,0 @@
-#include "../uint32.h"
-#include "../pe.h"
-
-pe_file_header*
-pe_filehdr_ptr(const void* ptr) {
-  const pe_dos_header* dos = ptr;
-  uint32 offset = uint32_get(&dos->e_lfanew);
-  return (pe_file_header*)((unsigned char*)ptr + offset);
-}
--- lib/pe/pe_opthdr_ptr.c
+++
@@ -1,9 +0,0 @@
-#include "../uint32.h"
-#include "../pe.h"
-
-void*
-pe_opthdr_ptr(const void* ptr) {
-  char* p = (char*)pe_filehdr_ptr(ptr);
-  p += sizeof(pe_file_header) + sizeof(uint32);
-  return p;
-}
