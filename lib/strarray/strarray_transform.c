c991f1750e1043f961d7aebd7cc8dcbb1be1a806
Roman Senn <roman.l.senn@gmail.com>
2018-10-13 09:51:15 +0200

    ...

---
+++ lib/strarray/strarray_transform.c
@@ -0,0 +1,20 @@
+#include "../str.h"
+#include "../stralloc.h"
+#include "../strarray.h"
+
+int
+strarray_transform(strarray* arr, char* (*pred)(const char*)) {
+  size_t i, n = strarray_size(arr);
+  char** p = array_start(arr);
+  for(i = 0; i < n; ++i) {
+    char* s = *p;
+    char* o = pred(s);
+
+    if(str_diff(s, o)) {
+      *p = o;
+      free(s);
+    }
+    ++p;
+  }
+  return 1;
+}
