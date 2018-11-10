eb1e2fd622feb1816cacba8c9af543e691f71455
Roman Senn <roman.l.senn@gmail.com>
2018-11-10 18:07:03 +0100

    ...

---
+++ lib/elf/elf_symbol_r.c
@@ -0,0 +1,13 @@
+#include "../elf.h"
+
+range
+elf_symbol_r(void* elf, void* sym) {
+  range r, t = elf_get_section_r(elf, ".text");
+  
+  r.start = t.start + ELF_GET(elf, sym, sym, st_value);
+  r.elem_size = ELF_GET(elf, sym, sym, st_size);
+  r.end = r.start + r.elem_size;
+
+  return r;
+
+}
