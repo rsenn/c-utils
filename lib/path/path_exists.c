3e62fa690ef8a80ba14024a30abd88a99e6f8932
Roman Senn <roman.l.senn@gmail.com>
2018-12-13 21:36:14 +0100

    ...

--- lib/path/path_exists.c
+++ lib/path/path_exists.c
@@ -1,18 +1,23 @@
 #include "../path_internal.h"
 #include "../windoze.h"
 
-#if !WINDOWS_NATIVE
+#if WINDOWS_NATIVE
+#include <io.h>
+#include <sys/stat.h>
+#define lstat stat
+#else
 #include <sys/stat.h>
 #endif
 
 int
 path_exists(const char* p) {
-  int ret;
 #if WINDOWS_NATIVE
-  ret = access(p, 0) == 0;
-#else
-  struct stat st;
-  ret = lstat(p, &st) == 0;
+  if(access(p, 0) == 0) return 1;
 #endif
-  return ret;
+  {
+    struct stat st;
+    if(lstat(p, &st) == 0)
+      return 1;
+  }
+  return 0;
 }
