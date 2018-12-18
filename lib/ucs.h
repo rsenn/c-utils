148a94d5101eba57e5cb570a1e4d64ef0a88418c
Roman Senn <roman.l.senn@gmail.com>
2018-12-18 15:44:25 +0000

    ...

---
+++ lib/ucs.h
@@ -0,0 +1,15 @@
+#ifndef UCS_H
+#define UCS_H
+
+#include <wchar.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+wchar_t ucs_latin1(int ch);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* defined UCS_H */
