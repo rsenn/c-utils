f77625c1cf272c79e83989e0d581047a8c13051b
Roman Senn <roman.l.senn@gmail.com>
2019-04-06 23:46:35 +0200

    ...

--- jsonpp.c
+++ jsonpp.c
@@ -57,9 +57,9 @@ if(depth < 2) {
   buffer_puts(buffer_2, ")");
   buffer_putnlflush(buffer_2);
 }*/
-  p->newline = (!one_line  && depth < 5 && ((index > -1) || index == -2)) ? "\n" : "";
+  p->newline = (!one_line  && valdepth > 1 && ((index > -1) || index == -2)) ? "\n" : "";
   p->indent = indent_str.s;
-  p->spacing = (!compact && index > -1) ? " " : "";
+  p->spacing = ((valdepth < 1 && index > 0) || (valdepth >= 1 &&  index > -1)) ? " " : "";
   p->quote = quote_char;
 };
 
