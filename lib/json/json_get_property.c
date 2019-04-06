3912c1c4fed0503a70b28dc02ce410e5fc6d0b30
Roman Senn <roman.l.senn@gmail.com>
2019-04-07 00:38:36 +0200

    ...

--- lib/json/json_get_property.c
+++ lib/json/json_get_property.c
@@ -7,8 +7,19 @@ json_get_property(jsonval* obj, jsonval name) {
     stralloc key;
     stralloc_init(&key);
     json_tostring(&name, &key);
+    stralloc_nul(&key);
     ret = hmap_get(&obj->dictv, key.s, key.len);
     stralloc_free(&key);
+  } else if(obj->type == JSON_ARRAY) {
+    struct slink* p;
+    int64 index = json_toint(name);
+
+    for(p = obj->listv; p; p = p->next) {
+      if(index-- == 0)
+        ret = slist_data(p);
+        break;
+      }
+    }
   }
   return ret;
 }
