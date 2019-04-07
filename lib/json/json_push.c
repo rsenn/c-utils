e080efbe50b8abe1c1d7f39a67414ade639adbab
Roman Senn <roman.l.senn@gmail.com>
2019-04-07 03:19:54 +0200

    ...

---
+++ lib/json/json_push.c
@@ -0,0 +1,14 @@
+#include "../slist.h"
+#include "../json.h"
+
+jsonval*
+json_push(jsonval* arr, jsonval item) {
+  if(arr->type == JSON_ARRAY) {
+    slink* new = slink_new(jsonval);
+    jsonval* val = slist_data(new);
+    slist_push(&arr->listv, new);
+    *val = item;
+    return val;
+  }
+  return 0;
+}
