11bfff26ba5e5fe53354753c9487873635b8365d
Roman Senn <roman.l.senn@gmail.com>
2019-09-16 01:25:42 +0200

    ...

--- lib/ihex.h
+++ lib/ihex.h
@@ -2,12 +2,23 @@
 #define IHEX_H_
 
 #include "uint8.h"
+#include "uint16.h"
+#include "slist.h"
+
+struct ihex_record_s;
 
 typedef struct {
+  struct ihex_record_s* next;
   uint8 reclen;
   uint16 offset;
   uint8 rectyp;
-  
+  uint8 checksum;
+  uint8 data[];  
 } ihex_record;
+
+typedef struct {
+  ihex_record* records;
+} ihex_file;
+
 
 #endif /* IHEX_H_ */
---
+++ lib/ihex/ihex_read_record.c
@@ -0,0 +1,7 @@
+#include "../ihex.h"
+
+
+ssize_t
+ihex_read_record(ihex_file* ihf, const char* x, size_t n) {
+  
+}
