ac591a14530331004d883685455fe163b59fdaaa
Roman Senn <roman.l.senn@gmail.com>
2019-12-27 02:30:45 +0100

    ...

--- scripts/do-genmakefile.sh
+++ scripts/do-genmakefile.sh
@@ -7,23 +7,34 @@ cd "$THISDIR/.."
 PROJECTDIR=$PWD
 PROJECTNAME=${PROJECTDIR##*/}
 
+count() {
+  (IFS=" "; eval "set -- $@"; echo $#)
+}
 get_num() {
- (IFS=" "; N=$1; shift; set -- $@
+ (IFS=" "; N=$1; shift; eval "set -- $@"
   eval 'echo "${'$N'}"')
 }
 
-BUILD_TOOLS="nmake pomake make gmake batch ninja"
-EXTENSIONS="nmake pomk mk gmake bat ninja"
-FILENAMES="NMakefile Makefile Makefile GNUmakefile build.bat build.ninja"
-
-case `uname -s` in
-  *_NT-*) : ${COMPILERS="bcc55 bcc32 dmc32 pocc32 pocc64 tcc32 tcc64 lcc32 lcc64 occ32"} ;;
-  *) : ${COMPILERS="gcc clang tcc zapcc"} ;;
-esac
-
 : ${BUILDDIR="build/\$C"}
 
-: ${FILENAME="\$FN"}
+: ${FILENAME="\$FN\${EXT:+.\$EXT}"}
+OUTPUT_FILE="${BUILDDIR:+$BUILDDIR/}$FILENAME"
+
+: ${SYSTEM=`uname -s`}
+case $SYSTEM in
+  *_NT-* | NT) 
+    : ${COMPILERS="bcc55 bcc32 dmc32 pocc32 pocc64 tcc32 tcc64 lcc32 lcc64 occ32"}
+BUILD_TOOLS="pomake batch ninja"
+EXTENSIONS="mk bat ninja"
+FILENAMES="\${C} \${C} \${C}"
+OUTPUT_FILE="$FILENAME"
+    ;;
+  *) : ${COMPILERS="gcc clang tcc zapcc"}
+BUILD_TOOLS="nmake pomake make gmake batch ninja"
+EXTENSIONS="'' '' '' '' bat ninja"
+FILENAMES="NMakefile Makefile Makefile GNUmakefile build build"
+;;
+esac
 
 set -f
 set -- lib *.c 3rdparty tests
@@ -33,15 +44,15 @@ set -- "$@" -DHAVE_{ZLIB,LIBBZ2,LIBLZMA}=1
 set -- genmakefile --minsizerel "$@" 
 
 : ${CMD=echo}
-
+echo Count: $(count "$BUILD_TOOLS") 1>&2
 for C in $COMPILERS; do 
-  for N in $(seq 1 5); do
+  for N in $(seq 1 $(count "$BUILD_TOOLS")); do
 
     MAKE=`get_num $N $BUILD_TOOLS`
     EXT=`get_num $N $EXTENSIONS`
     FN=`get_num $N $FILENAMES`
 
-   GENMK="$CMD $* -t $C -m $MAKE -o ${BUILDDIR:+$BUILDDIR/}$FILENAME"
+   GENMK="$CMD $* -t $C -m $MAKE -o $OUTPUT_FILE"
    #echo "GENMK='$GENMK'" 1>&2
    (set -f
    IFS=" "
