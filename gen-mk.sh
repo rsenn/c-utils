06c0b8aa48ce924a991b039bb901f67f58f50c87
Roman <roman.l.senn@gmail.com>
2020-03-11 20:37:13 +0100

    ...

--- gen-mk.sh
+++ gen-mk.sh
@@ -11,7 +11,7 @@ exec_cmd() {
 
 PROGRAMS="ar-wrap.c binfmttest.c bsdiffcat.c buffertest.c ccat.c cc-wrap.c cmake-run.c cofflist.c compiler-wrapper.c count-depth.c crc.c decode-ls-lR.c dnsip.c dnsname.c dnstest.c eagle-gen-cmds.c eagle-init-brd.c eagle-to-circuit.c eagle-to-svg.c elf64list.c elflist.c elfwrsec.c genmakefile.c hexedit.c httpproxy.c httptest.c impgen.c jsonpp.c jsontest.c list-r.c macho32list.c mediathek-list.c mediathek-parser.c msys-shell.c ntldd.c omflist.c opensearch-dump.c parse.c pathtool.c pelist.c piccfg.c pkgcfg.c plsconv.c rdir-test.c reg2cmd.c regfilter.c sln.c strarraytest.c tcping.c testihex.c torrent-progress.c xml2json.c xml2moon.c xmlpp.c xmltest.c xmltest2.c xmltest3.c xmltest4.c ziptest.c"
 
-MODULES="alloc,array,binfmt,buffer,byte,case,cb,cbmap,charbuf,coff,dir,dns,elf,env,errmsg,expand,fmt,gpio,hashmap,hmap,http,iarray,ihex,io,json,list,map,mmap,ndelay,omf,open,path,pe,playlist,process,range,rdir,safemult,scan,slist,socket,str,stralloc,strarray,strlist,tai,taia,textbuf,textcode,ucs,uint16,uint32,uint64,unix,var,vartab,wait,xml"
+MODULES="alloc,array,binfmt,buffer,byte,case,cb,cbmap,charbuf,coff,dir,dns,elf,env,errmsg,fmt,gpio,hashmap,hmap,http,iarray,ihex,io,json,list,map,mmap,ndelay,omf,open,path,pe,playlist,process,range,rdir,safemult,scan,slist,socket,str,stralloc,strarray,strlist,tai,taia,textbuf,textcode,ucs,uint16,uint32,uint64,unix,wait,xml"
 
 LIBS="-lws2_32"
 
