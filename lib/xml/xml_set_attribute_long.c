d3226947514104be125c47d792e7fe9a748546dd
Roman <roman.l.senn@gmail.com>
2020-05-05 06:18:00 +0200

    ...

---
+++ lib/xml/xml_set_attribute_long.c
@@ -0,0 +1,15 @@
+#include "../xml.h"
+#include "../scan.h"
+
+long
+xml_get_attribute_long(xmlnode* node, const char* attr) {
+  const char* str;
+
+  if((str = xml_get_attribute(node, attr))) {
+    long ret = ~0;
+    size_t n;
+    if((n = scan_long(str, &ret)))
+      return ret;
+  }
+  return 0;
+}
