bb37bb6e3ae48cdd3207b5dff8c17b309dd6a86e
Roman <roman.l.senn@gmail.com>
2020-09-23 12:49:02 +0200

    ...

--- tinyproxy.c
+++ tinyproxy.c
@@ -36,6 +36,7 @@
 #include "lib/uint16.h"
 #include "lib/uint64.h"
 #include "lib/stralloc.h"
+#include "lib/strlist.h"
 #include "lib/buffer.h"
 #include "lib/ip4.h"
 #include "lib/ip6.h"
@@ -122,36 +123,41 @@ typedef struct connection_s {
 } connection_t;
 
 static slink* connections;
+ dns_response_t* dns_query(stralloc*);
+void            dns_print(buffer*, dns_response_t* result, size_t num_responses);
+dns_response_t* dns_lookup(stralloc*);
 
-size_t dump_fds(array*);
-void dump_io(void);
+size_t          dump_fds(array*);
+void            dump_io(void);
 
-connection_t* connection_new(fd_t, char addr[16], uint16 port);
-void connection_delete(connection_t*);
-connection_t* connection_find(fd_t, fd_t proxy);
-fd_t connection_open_log(connection_t*, const char* prefix, const char* suffix);
+connection_t*   connection_new(fd_t, char addr[16], uint16 port);
+void            connection_delete(connection_t*);
+connection_t*   connection_find(fd_t, fd_t proxy);
+fd_t            connection_open_log(connection_t*, const char* prefix, const char* suffix);
 
-socketbuf_t* socket_find(fd_t);
-socketbuf_t* socket_other(fd_t);
-ssize_t socket_send(fd_t, void* x, size_t n, void* ptr);
-int socket_connect(socketbuf_t*);
-void socket_accept(fd_t, char addr[16], uint16 port);
+socketbuf_t*    socket_find(fd_t);
+socketbuf_t*    socket_other(fd_t);
+ssize_t         socket_send(fd_t, void* x, size_t n, void* ptr);
+int             socket_connect(socketbuf_t*);
+void            socket_accept(fd_t, char addr[16], uint16 port);
 
-void sockbuf_init(socketbuf_t*);
-size_t sockbuf_fmt_addr(socketbuf_t*, char* dest, char sep);
-void sockbuf_put_addr(buffer*, socketbuf_t* sb);
-void sockbuf_close(socketbuf_t*);
-void sockbuf_check(socketbuf_t*);
-void sockbuf_log_data(socketbuf_t*, bool send, char* x, ssize_t len);
-ssize_t sockbuf_forward_data(socketbuf_t*, socketbuf_t* destination);
+void            sockbuf_init(socketbuf_t*);
+size_t          sockbuf_fmt_addr(socketbuf_t*, char* dest, char sep);
+void            sockbuf_put_addr(buffer*, socketbuf_t* sb);
+void            sockbuf_close(socketbuf_t*);
+void            sockbuf_check(socketbuf_t*);
+void            sockbuf_log_data(socketbuf_t*, bool send, char* x, ssize_t len);
+ssize_t         sockbuf_forward_data(socketbuf_t*, socketbuf_t* destination);
 
-fd_t server_socket(void);
-fd_t server_listen(uint16);
-void server_sigterm(int);
-void server_loop(void);
-void server_connection_count(void);
+fd_t            server_socket(void);
+fd_t            server_listen(uint16);
+void            server_exit(int);
+void            server_sigint(int);
+void            server_sigterm(int);
+void            server_loop(void);
+void            server_connection_count(void);
 
-void usage(const char*);
+void            usage(const char*);
 
 static socketbuf_t server, remote;
 
@@ -164,6 +170,8 @@ static char logbuf[1024];
 static buffer log = BUFFER_INIT(write, STDOUT_FILENO, logbuf, sizeof(logbuf));
 static MAP_T dns_cache;
 static tai6464 ttl;
+static strlist output_files;
+
 
 #define BACKLOG 20 // how many pending connections queue will hold
 
@@ -252,7 +260,7 @@ dns_lookup(stralloc* h) {
 
   if(cached)
     range_rotate(&result->data, -1);
-
+#ifdef DEBUG_OUTPUT_
   buffer_puts(buffer_2, cached ? "Cache hit " : "Resolved ");
   buffer_putsa(buffer_2, h);
   if(cached) {
@@ -265,7 +273,7 @@ dns_lookup(stralloc* h) {
   buffer_puts(buffer_2, "] ");
   dns_print(buffer_2, result, cached ? 1 : range_size(&result->data));
   buffer_putnlflush(buffer_2);
-
+#endif
   return result;
 }
 
@@ -439,6 +447,8 @@ connection_open_log(connection_t* c, const char* prefix, const char* suffix) {
       buf[i] = '+';
   }
   buf[i] = '\0';
+  strlist_push_unique(&output_files, buf);
+
   return open_trunc(buf);
 }
 
@@ -744,6 +754,21 @@ server_listen(uint16 port) {
 }
 
 void
+server_exit(int code) {
+  buffer_puts(&log, "Output files: ");
+  strlist_dump( &log, &output_files);
+  buffer_putnlflush(&log);
+  buffer_close(&log);
+  exit(code);
+}
+
+void
+server_sigint(int sig) {
+  buffer_puts(&log, "SIGINT received");
+  buffer_putnlflush(&log);
+  server_exit(1);
+}
+void
 server_sigterm(int sig) {
   buffer_puts(&log, "SIGTERM received");
   buffer_putnlflush(&log);
@@ -1041,6 +1066,7 @@ main(int argc, char* argv[]) {
 
   sig_block(SIGPIPE);
   sig_catch(SIGTERM, &server_sigterm);
+sig_catch(SIGINT, &server_sigint);
 
   if((server_sock = server_listen(server.port)) < 0) { // start server
     errmsg_warn("Cannot run server:", 0);
