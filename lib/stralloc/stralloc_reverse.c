48f1b4ffbdae326e7be599bd1d4443b3ee55a9e4
Roman <roman.l.senn@gmail.com>
2020-11-22 21:00:35 +0100

    ...

--- lib/stralloc/stralloc_reverse.c
+++ lib/stralloc/stralloc_reverse.c
@@ -6,7 +6,7 @@
 void
 stralloc_reverse(stralloc* sa) {
   size_t i = 0;
-  for(; i<sa->len> > 1; i++) {
+  for(; i < sa->len >> 1; i++) {
     char tmp = sa->s[i];
     sa->s[i] = sa->s[sa->len - 1 - i];
     sa->s[sa->len - 1 - i] = tmp;
