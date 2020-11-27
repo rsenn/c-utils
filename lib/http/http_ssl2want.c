cfdb9988818084f72f220796e4c3f4bf65979d8d
Roman <roman.l.senn@gmail.com>
2020-11-27 07:04:53 +0100

    ...

---
+++ lib/http/http_ssl2want.c
@@ -0,0 +1,27 @@
+#include "../http.h"
+
+#ifdef HAVE_OPENSSL
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <assert.h>
+#include <errno.h>
+
+ssize_t
+https_tls2want(http* h, ssize_t ret, void (*wantread)(fd_t), void (*wantwrite)(fd_t)) {
+  if(ret <= 0) {
+    switch(SSL_get_error(h->ssl, ret)) {
+      case SSL_ERROR_WANT_READ:
+        assert(wantread);
+        wantread(h->sock);
+        break;
+      case SSL_ERROR_WANT_WRITE:
+        assert(wantwrite);
+        wantwrite(h->sock);
+        break;
+      default: break;
+    }
+  }
+  return ret;
+}
+#endif
+  
---
+++ lib/ssl/ssl_accept.c
@@ -0,0 +1,20 @@
+#include "../tls_internal.h"
+#include <assert.h>
+
+#ifdef HAVE_OPENSSL
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+int
+tls_accept(fd_t fd) {
+  tls_instance* i = iarray_get(&tls_list, fd);
+  assert(i);
+  assert(i->ssl);
+
+  if(tls_instance_return(i, SSL_accept(i->ssl)) == 1)
+    return 1;
+  if((errno = tls_instance_errno(i)))
+    return -1;
+  return 0;
+}
+#endif
