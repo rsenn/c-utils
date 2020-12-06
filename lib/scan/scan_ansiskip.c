ffbc85bb491289055c0f2274868838743bd9f44d
Roman <roman.l.senn@gmail.com>
2020-12-06 11:40:56 +0100

    ...

--- lib/scan/scan_ansiskip.c
+++ lib/scan/scan_ansiskip.c
@@ -7,8 +7,9 @@ scan_ansiskip(const char* s, size_t limit) {
   const char* u = t + limit;
 
   if(*t == 0x1b) {
-    ;
-    if((++t < u) && *t == '[') {
+    ++t;
+    if( t < u && *t == '[') {
+    //  ++t;
       while(t < u && !isalpha(*t)) ++t;
       return (size_t)(t - s);
     }
