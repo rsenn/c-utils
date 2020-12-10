72b3949081e00290d63666800b314ec07895656f
Roman <roman.l.senn@gmail.com>
2020-12-10 04:18:59 +0100

    ...

--- lib/buffer/buffer_put_escaped.c
+++ lib/buffer/buffer_put_escaped.c
@@ -1,15 +1,12 @@
 #include "../buffer.h"
-#include "../byte.h"
-#include "../fmt.h"
-#include "../stralloc.h"
-#include <ctype.h>
-
+  
 int
 buffer_put_escaped(buffer* b, const char* x, size_t len, size_t (*escape)(char*, int)) {
   char buf[16];
   size_t i, n, r = 0;
   for(i = 0; i < len; i++) {
-    n = escape(buf, x[i]);
+  	unsigned int c = x[i];
+    n = escape(buf,c);
     buffer_put(b, buf, n);
     r += n;
   }
