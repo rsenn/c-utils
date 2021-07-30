65a9be1036095bf626ef6da20c9d417d458fd7ca
Roman Senn <roman.l.senn@gmail.com>
2021-07-31 01:42:28 +0200

    ...

--- lib/sig/sig_catch.c
+++ lib/sig/sig_catch.c
@@ -4,6 +4,11 @@
 
 int
 sig_catch(int sig, sighandler_t_ref f) {
-  struct sigaction ssa = {.sa_handler = f, .sa_mask = {{0}}, .sa_flags = SA_MASKALL | SA_NOCLDSTOP, .sa_restorer = 0};
+  struct sigaction ssa = {.sa_handler = f, .sa_mask = {{0}}, .sa_flags = SA_MASKALL | SA_NOCLDSTOP
+#if !defined(_WIN32) && !defined(_WIN64) && !defined(__MSYS__)
+	  , .sa_restorer = 0
+#endif
+
+  };
   return sig_catcha(sig, &ssa);
 }
