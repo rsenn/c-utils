23d89b7ff302809fe3535aea8dce17cdeb53970d
Roman Senn <roman.l.senn@gmail.com>
2021-07-31 07:21:32 +0200

    ...

--- cmake/CheckZlib.cmake
+++ cmake/CheckZlib.cmake
@@ -1,46 +1,28 @@
 if(USE_ZLIB)
-
   if(NOT ZLIB_LIBRARY)
-
     message(STATUS "Checking for libz")
-    find_library(ZLIB z)
-
-    message(STATUS "${ZLIB}")
-
-    set(ZLIB_LIBRARY "${ZLIB}")
-
-    if(ZLIB)
+    find_library(ZLIB_LIBARY z)
+    message(STATUS "${ZLIB_LIBARY}")
+    if(ZLIB_LIBARY)
       set(ZLIB_FOUND TRUE)
       if(ZLIB STREQUAL "")
         set(ZLIB z)
       endif(ZLIB STREQUAL "")
-      set(ZLIB_LIBARY_RELEASE "${ZLIB}" CACHE FILEPATH "")
-      set(ZLIB_LIBARY_DEBUG "${ZLIB}" CACHE FILEPATH "")
-      set(ZLIB_LIBARY "${ZLIB}" CACHE FILEPATH "")
-    endif(ZLIB)
-
+    endif(ZLIB_LIBARY)
     if(NOT ZLIB_FOUND)
-
-      pkg_search_module(ZLIB zlib libz z)
+      pkg_search_module(ZLIB_LIBARY zlib libz z)
       if(ZLIB_FOUND)
         if(pkgcfg_lib_ZLIB_z)
-          set(ZLIB_LIBRARY_RELEASE "${pkgcfg_lib_ZLIB_z}" CACHE FILEPATH "")
-          set(ZLIB_LIBRARY_DEBUG "${pkgcfg_lib_ZLIB_z}" CACHE FILEPATH "")
           set(ZLIB_LIBRARY "${pkgcfg_lib_ZLIB_z}" CACHE FILEPATH "")
         endif(pkgcfg_lib_ZLIB_z)
       else(ZLIB_FOUND)
         # include(FindZlib)
       endif(ZLIB_FOUND)
-
     endif(NOT ZLIB_FOUND)
   endif(NOT ZLIB_LIBRARY)
-
   if(ZLIB_LIBRARY)
     message(STATUS "zlib library found: ${ZLIB_LIBRARY}")
   endif(ZLIB_LIBRARY)
-
 else(USE_ZLIB)
-  unset(ZLIB_LIBRARY_RELEASE CACHE)
-  unset(ZLIB_LIBRARY_DEBUG CACHE)
   unset(ZLIB_LIBRARY CACHE)
 endif(USE_ZLIB)
