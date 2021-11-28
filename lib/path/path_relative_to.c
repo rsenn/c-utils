814714f2a9c8fd71987ca45d130d22beae301516
Roman Senn <roman.l.senn@gmail.com>
2021-11-28 20:00:58 +0100

    ...

--- lib/path/path_relative_to.c
+++ lib/path/path_relative_to.c
@@ -4,8 +4,3 @@ int
 path_relative_to(const char* path, const char* relative_to, stralloc* out) {
   return path_relative_to_b(path, str_len(path), relative_to, str_len(relative_to), out);
 }
-
-int
-path_relative_to_sa(const stralloc* path, const stralloc* relative_to, stralloc* out) {
-  return path_relative_to_b(path->s, path->len, relative_to->s,relative_to->len, out);
-}
