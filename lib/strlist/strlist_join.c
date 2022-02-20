bd2ef4b1888dc67b5f42d5f62154e64c22644286
Roman Senn <roman.l.senn@gmail.com>
2022-02-20 04:34:54 +0100

    ...

--- lib/strlist/strlist_join.c
+++ lib/strlist/strlist_join.c
@@ -4,9 +4,14 @@
 
 void
 strlist_join(const strlist* sl, stralloc* sa, char delim) {
-  const char* x;
+  const char* x, *y;
   size_t i = 0, n;
-  strlist_foreach(sl, x, n) {
+
+  for(x = sl->sa.s, y=sl->sa.s+sl->sa.len; x < y; ) {
+//  strlist_foreach(sl, x, n) {
+//  
+   n = byte_chr(x, y-x, sl->sep);
+
     if(i)
       stralloc_catc(sa, delim);
     stralloc_catb(sa, x, n);
