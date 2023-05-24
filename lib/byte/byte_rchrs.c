0cfa25c089b307be60999618eb28a0c1478e7d16
Roman Senn <roman.l.senn@gmail.com>
2023-05-24 23:34:27 +0200

    ...

--- lib/byte/byte_rchrs.c
+++ lib/byte/byte_rchrs.c
@@ -1,13 +1,15 @@
 #include "../byte.h"
 
 size_t
-byte_rchrs(const void* b, size_t len, const char* needles, size_t nn) {
-  const char *x = b, *found = NULL;
-  size_t i, j;
-  for(i = 0; i < len; i++) {
-    for(j = 0; j < nn; ++j)
-      if(x[i] == needles[j])
-        found = x;
+byte_rchrs(const void* in, size_t len, const char needles[], size_t nn) {
+  const char *s = in, *e = in + len, *found = NULL;
+  size_t i;
+  for(; s < e;) {
+    for(i = 0; i < nn; ++i) {
+      if(*s == needles[i])
+        found = s;
+    }
+    ++s;
   }
-  return i;
+  return (size_t)((found ? found : s) - (const char*)in);
 }
