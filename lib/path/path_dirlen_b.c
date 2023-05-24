88b3ab2eb2dcbd274ff41a5c6c43e31bc61d2eb5
Roman Senn <roman.l.senn@gmail.com>
2023-05-24 23:29:50 +0200

    ...

--- lib/path/path_dirlen_b.c
+++ lib/path/path_dirlen_b.c
@@ -3,5 +3,8 @@
 
 size_t
 path_dirlen_b(const char* path, size_t size) {
-  return byte_rchrs(path, size, PATHSEP_S_MIXED, sizeof(PATHSEP_S_MIXED) - 1);
+  const char ps[] = PATHSEP_S_MIXED;
+  const size_t pslen = sizeof(ps) - 1;
+
+  return byte_rchrs(path, size, ps, pslen);
 }
