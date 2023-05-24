e9a148239578f2edaab5e492f6a32f84393e9231
Roman Senn <roman.l.senn@gmail.com>
2023-05-24 22:10:20 +0200

    ...

---
+++ lib/process/process_executable.c
@@ -0,0 +1,27 @@
+#include "../process.h"
+
+char*
+process_executable(void) {
+  ssize_t len;
+stralloc prog;
+
+  stralloc_init(&prog);
+
+#ifndef WINDOWS_NATIVE
+  stralloc_ready(&prog, PATH_MAX);
+
+  if((len = readlink("/proc/self/exe", prog.s, prog.a)) > 0) {
+    prog.len = len;
+    return prog.s;
+  }
+#endif
+
+#if WINDOWS
+  if((len = GetModuleFileNameA(0, prog.s, prog.a)) > 0) {
+    prog.len = len;
+    return prog.s;
+  }
+#endif
+
+  return 0;
+}
