1e967f3cc2832fb876db2ad551fab33b9a0e3a5c
Roman Senn <roman.l.senn@gmail.com>
2023-05-27 01:30:16 +0200

    ...

--- lib/cpp/cpp_new.c
+++ lib/cpp/cpp_new.c
@@ -3,6 +3,7 @@
 cpp_t*
 cpp_new(void) {
   cpp_t* ret;
+  struct macro_s m = {.num_args = 1};
 
   if(!(ret = alloc_zero(sizeof(struct cpp_s))))
     return ret;
@@ -11,13 +12,10 @@ cpp_new(void) {
 
   MAP_NEW(ret->macros);
 
-  {
-    struct macro_s m = {.num_args = 1};
-    cpp_add_macro(ret, str_dup("defined"), &m);
-    m.num_args = MACRO_FLAG_OBJECTLIKE;
-    cpp_add_macro(ret, str_dup("__FILE__"), &m);
-    cpp_add_macro(ret, str_dup("__LINE__"), &m);
-  }
+  cpp_add_macro(ret, str_dup("defined"), &m);
+  m.num_args = MACRO_FLAG_OBJECTLIKE;
+  cpp_add_macro(ret, str_dup("__FILE__"), &m);
+  cpp_add_macro(ret, str_dup("__LINE__"), &m);
 
   return ret;
 }
