a51280b32e3586c568fb3305564ebd1b6b7d8e57
Roman Senn <roman.l.senn@gmail.com>
2023-12-31 01:51:44 +0100

    ...

--- lib/stralloc/stralloc_ready.c
+++ lib/stralloc/stralloc_ready.c
@@ -12,20 +12,28 @@ int
 stralloc_ready(stralloc* sa, size_t len) {
   size_t wanted = len + (len >> 3) + 30; /* heuristic from djb */
   char* tmp;
+
   if(sa->s && sa->a >= len)
     return 1;
+
   if(sa->a == 0 || sa->s == NULL) {
+
     if(!(tmp = malloc(wanted)))
       return 0;
+
     if(sa->s)
       byte_copy(tmp, len, sa->s);
     else
       byte_zero(tmp, wanted);
+
   } else {
+
     if(!(tmp = realloc(sa->s, wanted)))
       return 0;
   }
+
   sa->a = wanted;
   sa->s = tmp;
+
   return 1;
 }
