69fa8846ce3cc9a2db7faa468302b3d98d32d4ba
Roman Senn <roman.l.senn@gmail.com>
2025-03-29 18:11:27 +0100

    ...

--- lib/scan/scan_xlongn.c
+++ lib/scan/scan_xlongn.c
@@ -1,13 +1,14 @@
 #include "../scan.h"
 #include "../str.h"
+#include <ctype.h>
 
 size_t
 scan_xlongn(const char* src, size_t n, unsigned long* dest) {
   const char* tmp = src;
   unsigned long l = 0;
-  int c;
+  unsigned char c;
 
-  while(n-- > 0 && (l >> (sizeof(l) * 8 - 4)) == 0 && str_has("0123456789abcdefABCDEF", *tmp) && (c = scan_fromhex((unsigned char)*tmp)) >= 0) {
+  while(n-- > 0 && (l >> (sizeof(l) * 8 - 4)) == 0 && (c = str_chr("0123456789abcdef", tolower(*tmp))) < 16) {
     l = (l << 4) + (unsigned char)c;
     ++tmp;
   }
